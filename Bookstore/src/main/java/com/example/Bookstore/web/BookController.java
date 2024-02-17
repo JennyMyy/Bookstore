8c9b8979a87ddac9714b14584851b043fe71bf54
JennyMyy <jenny.mattila@elisanet.fi>
2024-02-17 16:16:26 +0200

    style

--- Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
@@ -35,6 +35,14 @@ public class BookController {
         return "redirect:booklist";
     } 
 
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+    	model.addAttribute("book", repository.findById(bookId));
+        model.addAttribute("departments", repository.findAll());
+        return "edit";
+    }
+
+
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
