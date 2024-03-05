8673802f1a1c82d0b0baf94e7abb288f23fe72f6
JennyMyy <jenny.mattila@elisanet.fi>
2024-03-05 14:30:26 +0200

    Rest added

--- Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package com.example.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.Bookstore.model.Book;
 import com.example.Bookstore.model.BookRepository;
 import com.example.Bookstore.model.CategoryRepository;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BookController {
@@ -20,37 +23,55 @@ public class BookController {
     @Autowired
     private CategoryRepository carepository;
 
+    // Shows all books
     @RequestMapping(value = "/booklist", method = RequestMethod.GET)
     public String ShowIndex(Model model) {
-       model.addAttribute("books", repository.findAll());
+        model.addAttribute("books", repository.findAll());
         return "booklist";
     }
 
+    // Restfulservice to get all books
+    @RequestMapping(value="/books", method=RequestMethod.GET)
+        public @ResponseBody List<Book> booklistRest() {
+            return(List<Book>) repository.findAll();
+        }
+    
+    // Restful service to get student by id
+    @RequestMapping(value="/book/{id}", method=RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+        return repository.findById(bookId);
+    }
+    
+    
+    // Add books
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String addBook(Model model) {
-       model.addAttribute("book", new Book());
-       model.addAttribute("categories", carepository.findAll());
+        model.addAttribute("book", new Book());
+        model.addAttribute("categories", carepository.findAll());
         return "addbook";
     }
 
+    // Save a new book
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(Book book){
+    public String save(Book book) {
         repository.save(book);
         return "redirect:booklist";
-    } 
+    }
 
+    // Edit selected book
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
     public String editBook(@PathVariable("id") Long bookId, Model model) {
-    	model.addAttribute("book", repository.findById(bookId));
+        model.addAttribute("book", repository.findById(bookId));
         model.addAttribute("departments", repository.findAll());
         model.addAttribute("categories", carepository.findAll());
         return "edit";
     }
 
+    // Delete by book id
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
-    	repository.deleteById(bookId);
+        repository.deleteById(bookId);
         return "redirect:../booklist";
-    } 
+    }
 
 }
